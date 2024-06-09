925d60d4fd1557ece2bddb70b02a81a6e75e5136
Keshav Saraogi <tuj49866@temple.edu>
2024-06-10 00:39:32 +0530

    Login
    -- login component
    -- styling the login component

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import loginImage from '../../assets/images/login.svg'
+import './Auth.scss'
 
 const Login = () => {
     return (
-        <h1>Login Screen</h1>
+        <div id="auth-container">
+            <div id="auth-card">
+                <div id="card-shadow">
+                    <div id="image-selection">
+                        <img src = {loginImage} alt = "Login"></img>
+                    </div>
+                    <div id="form-selection">
+                        <h2>Welcome Back</h2>
+                        <form>
+                            <div className="input-field mb-1">
+                                <input placeholder="Email" />
+                            </div>
+                            <div className="input-field mb-2">
+                                <input placeholder="Password" />
+                            </div>
+
+                            <button>LOGIN</button>
+                        </form>
+                        <p>Dont Have An Account? Register</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
