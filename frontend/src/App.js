4500605aed4b442bfd86df5f67227381b8a87c20
Keshav Saraogi <tuj49866@temple.edu>
2024-06-09 22:26:44 +0530

    components and routes
    -- creating initial components
    -- adding basic routers
    -- handling page not found error
    --

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import React from 'react';
+import Login from './components/authentication/Login';
+import Register from './components/authentication/Register';
+import Chat from './components/chat/Chat';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <h1>CHAT APP</h1>
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<h1>404 Page Not Found</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
