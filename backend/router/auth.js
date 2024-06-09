80965420dbfd2aa0205d67c562ecb937d5babb21
Keshav Saraogi <tuj49866@temple.edu>
2024-06-09 20:08:50 +0530

    bugs
    -- fixing middleware components

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,10 +1,10 @@
-const validate = require('../validators')
-const router = require('express').Router()
-const {login, register} = require('../controllers/authController')
-const {rules: loginRules} = require('../validators/auth/login')
-const {rules: registerationRules} = require('../validators/auth/register')
+const validate = require('../validators');
+const router = require('express').Router();
+const { login, register } = require('../controllers/authController');
+const { rules: loginRules } = require('../validators/auth/login');
+const { rules: registerationRules } = require('../validators/auth/register');
 
-router.post('/login', [loginRules, validate], login)
-router.post('/register', [registerationRules, validate], register)
+router.post('/login', [loginRules, validate.validate], login);
+router.post('/register', [registerationRules, validate.validate], register);
 
-module.exports = router
+module.exports = router;
--- backend/validators/index.js
+++ backend/validators/index.js
@@ -1,11 +1,11 @@
-const {validationResult} = require('express-validator')
+const { validationResult } = require('express-validator');
 
-exports.validate = ((req, res, next) => {
-    const errors = validationResult(req)
+exports.validate = (req, res, next) => {
+    const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(404).json({errors: errors.array()})
+        return res.status(404).json({ errors: errors.array() });
     }
 
-    next()
-})()
+    next();
+};
