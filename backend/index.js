c6a2e8a9c37cd6f2a0445b1f34bc9392c0ad8ad6
Keshav Saraogi <tuj49866@temple.edu>
2024-06-10 14:16:05 +0530

    Register, Login Component
    -- adding routing links to components
    -- updating the user state with react
    -- authenticating with axios

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser')
 const config = require('./config/app')
 const router = require('./router')
 
+const cors = require('cors')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(router)
+app.use(cors())
 
 const port = 3000
     
--- frontend/src/components/authentication/Register.js
+++ frontend/src/components/authentication/Register.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import loginImage from '../../assets/images/login.svg'
+import './Auth.scss'
+import {Link} from 'react-router-dom'
 
-const Register = () => {
+const Login = () => {
     return (
-        <h1>Register Screen</h1>
+        <div id="auth-container">
+            <div id="auth-card">
+                <div id="card-shadow">
+                    <div id="image-selection">
+                        <img src = {loginImage} alt = "Login"></img>
+                    </div>
+                    <div id="form-selection">
+                        <h2>Welcome Back</h2>
+                        <form>
+                            <div className="input-field mb-1">
+                                <input placeholder="First Name" />
+                            </div>
+                            <div className="input-field mb-1">
+                                <input placeholder="Last Name" />
+                            </div>
+                            <div className="input-field mb-1">
+                                <input placeholder="Email" />
+                            </div>
+                            <div className="input-field mb-1">
+                                <select>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                </select>
+                            </div>
+
+                            <div className="input-field mb-2">
+                                <input placeholder="Password" />
+                            </div>
+                            <button>REGISTER</button>
+                        </form>
+                        <p>Already Have An Account? <Link to="/login">Login</Link></p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Register
+export default Login
