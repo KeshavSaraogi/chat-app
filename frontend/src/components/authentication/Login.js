38e5690f5c33a8c785c4659000f137d2305ac03a
Keshav Saraogi <tuj49866@temple.edu>
2024-06-18 00:24:32 +0530

    components

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -1,69 +1,64 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import loginImage from '../../assets/images/login.svg'
-import './Auth.scss'
-import {Link} from 'react-router-dom'
-import authService from '../../services/authService'
+import { Link } from 'react-router-dom'
+
 import { useDispatch } from 'react-redux'
 import { login } from '../../store/actions/auth'
 
-const Login = ({history}) => {
+import './Auth.scss'
+
+const Login = ({ history }) => {
+
     const dispatch = useDispatch()
 
-    const [email, setEmail] = useState('john.doe@gmail.com');
-    const [password, setPassword] = useState('secret');
+    const [email, setEmail] = useState('sam.smith@gmail.com')
+    const [password, setPassword] = useState('secret')
 
     const submitForm = (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        dispatch(login({email, password}, history))
-
-        //props.history
-        
-        // authService
-        // .login({email, password})
-        // .then(res => console.log(res))
-
-        // axios.post('http://127.0.0.1:3000/login', { email, password })
-        //     .then(res => {
-        //         console.log("res", res);
-        //     })
-        //     .catch(err => {
-        //         console.log("err", err);
-        //     });
-
-        // console.log({ email, password });
-    };
+        dispatch(login({ email, password }, history))
+    }
 
     return (
-        <div id="auth-container">
-            <div id="auth-card">
-                <div id="card-shadow">
-                    <div id="image-selection">
-                        <img src = {loginImage} alt = "Login"></img>
+        <div id='auth-container'>
+            <div id='auth-card'>
+                <div className='card-shadow'>
+                    <div id='image-section'>
+                        <img src={loginImage} alt='Login' />
                     </div>
-                    <div id="form-selection">
-                        <h2>Welcome Back</h2>
-                            <form onSubmit={submitForm}>
-                                <input 
-                                    type="email" 
-                                    value={email} 
-                                    onChange={(e) => setEmail(e.target.value)} 
-                                    placeholder="Email" 
-                                />
-                                <input 
-                                    type="password" 
-                                    value={password} 
-                                    onChange={(e) => setPassword(e.target.value)} 
-                                    placeholder="Password" 
-                                />
-                                <button type="submit">Login</button>
-                            </form>
-                        <p>Dont Have An Account? <Link to="/register">Register</Link></p>
+
+                    <div id='form-section'>
+                        <h2>Welcome back</h2>
+
+                        <form onSubmit={submitForm}>
+                            <div className='input-field mb-1'>
+                                <input
+                                    onChange={e => setEmail(e.target.value)}
+                                    value={email}
+                                    required='required'
+                                    type='text'
+                                    placeholder='Email' />
+                            </div>
+
+                            <div className='input-field mb-2'>
+                                <input
+                                    onChange={e => setPassword(e.target.value)}
+                                    value={password}
+                                    required='required'
+                                    type='password'
+                                    placeholder='Password' />
+                            </div>
+
+                            <button>LOGIN</button>
+                        </form>
+
+                        <p>Don't have an account? <Link to='/register'>Register</Link></p>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Login
--- frontend/src/components/chat/Chat.js
+++ frontend/src/components/chat/Chat.js
@@ -1,14 +1,26 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import useSocket from './hooks/socketConnect'
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
+import { fetchChats } from '../../store/actions/chat'
+import FriendList from './components/FriendList/FriendList'
+import Messenger from './components/Messenger/Messenger'
 import './Chat.scss'
 
 const Chat = () => {
 
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.authReducer.user)
+
+    useSocket(user, dispatch)
+
     return (
         <div id='chat-container'>
             <Navbar />
+            <div id='chat-wrap'>
+                <FriendList />
+                <Messenger />
+            </div>
         </div>
     );
 }
